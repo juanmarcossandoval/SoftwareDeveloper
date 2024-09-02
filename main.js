d80a2b9b5ecb25a3fe98564b1b5eae9993f21bf0
juanmarcossandoval <juanmarcos.sandoval@gmail.com>
2024-09-02 16:29:26 -0300

    fix: modal and inputs validations refactor

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const modal = document.getElementById("emailModal");
-const sending = document.getElementById("sending");
-const resultOK = document.getElementById("resultOK");
-const resultKO = document.getElementById("resultKO");
+const result = document.getElementById("result");
+const result_msg = document.getElementById("result-msg");
+const email_form = document.getElementById("emailForm");
+const btn_send = document.getElementById("send-btn");
+const subj_inp = document.getElementById("subject");
+const subj_lbl = document.getElementById("subj-lbl");
+const mail_inp = document.getElementById("email");
+const mail_lbl = document.getElementById("email-lbl");
+const msg_inp = document.getElementById("msg");
+const msg_lbl = document.getElementById("msg-lbl");
+const fullname_inp = document.getElementById("fullname");
+const fullname_lbl = document.getElementById("fullname-lbl");
 
 function openSection(evt, section) {
 
@@ -28,43 +37,96 @@ function openEmailModal(){
 }
 
 function closeEmailModal() {
-    document.getElementById("subject").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("msg").value = "";
-    document.getElementById("fullname").value = "";
-    resultOK.style.display = "none";
-    resultKO.style.display = "none";
-    sending.style.display = "none"
+    subj_inp.value = "";
+    mail_inp.value = "";
+    msg_inp.value = "";
+    fullname_inp.value = "";
+    result.style.display = "none";
+    result_msg.innerText = "";
+    email_form.style.display = "flex";
     modal.style.display = "none";
+    btn_send.setAttribute("disabled","");
 }
 
 function sendEmail(event) {
 
     event.preventDefault();
-    sending.style.display = "flex"; 
 
-    const asunto = document.getElementById("subject").value;
-    const mail = document.getElementById("email").value;
-    const mensaje = document.getElementById("msg").value;
-    const nombre = document.getElementById("fullname").value;
+    email_form.style.display = "none";
+    result.style.display = "flex";
+    result_msg.innerText = "Enviando email..";
 
-    var templateParams = {
-        subject: asunto,
-        name: nombre,
-        email: mail,
-        msg: mensaje,
-        reply_to: mail       
+    const templateParams = {
+        subject: subj_inp.value,
+        name: fullname_inp.value,
+        email: mail_inp.value,
+        msg: msg_inp.value,
+        reply_to: mail_inp.value       
     };
 
     emailjs.send('jms_portfolio_email', 'contacto_portfolio', templateParams).then(
         (response) => {
-            sending.style.display = "none";
-            resultOK.style.display ="flex";
+            result_msg.innerText = "¡Su correo fue enviado con éxito a juanmarcos.sandoval@gmail.com!";
         },
         (error) => {
-            sending.style.display = "none";
-            resultKO.style.display ="flex";
+            result_msg.innerText = "¡Su correo no pudo ser enviado a juanmarcos.sandoval@gmail.com!";
         },
     );
 
 }
+
+function calcularEdad() {
+    const hoy = new Date();
+    const cumpleanos = new Date('1990-12-20');
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    const m = hoy.getMonth() - cumpleanos.getMonth();
+
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+
+    document.getElementById("edad").innerText = `20-12-1990 ${edad} años`;
+}
+
+calcularEdad();
+
+function validateLength(value,label){
+    value.length == 0 ? label.classList.add("invalid-label") : label.classList.remove("invalid-label");
+    return value.length > 0;
+}
+
+function validateEmail(value,label){
+    let valid = false;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    emailRegex.test(value) ? 
+    (label.classList.remove("invalid-label"), valid = true) 
+    : label.classList.add("invalid-label");
+    return valid;
+}
+
+function validateFields(){
+    const validSubj = validateLength(subj_inp.value,subj_lbl);
+    const validMsg = validateLength(msg_inp.value,msg_lbl);
+    const validName = validateLength(fullname_inp.value,fullname_lbl);
+    const validMail = validateEmail(mail_inp.value,mail_lbl);
+
+    validMail && validMsg && validName && validSubj ? 
+    btn_send.removeAttribute("disabled") :
+    btn_send.setAttribute("disabled","");
+}
+
+subj_inp.addEventListener('keyup', (event) => {
+    validateFields();
+});
+
+msg_inp.addEventListener('keyup', (event) => {
+    validateFields();
+});
+
+fullname_inp.addEventListener('keyup', (event) => {
+    validateFields();
+});
+
+mail_inp.addEventListener('keyup', (event) => {
+    validateFields();
+});
